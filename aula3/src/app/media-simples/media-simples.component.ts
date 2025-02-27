7a5070b61fe61e066810ee1e8eb8614e4b3cb6b4
Luckvers <156406555+Luckvers@users.noreply.github.com>
2025-02-26 22:06:50 -0300

    Aula3

---
+++ aula3/src/app/media-simples/media-simples.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-simples',
+  imports: [],
+  templateUrl: './media-simples.component.html',
+  styleUrl: './media-simples.component.css'
+})
+export class MediaSimplesComponent {
+
+}
